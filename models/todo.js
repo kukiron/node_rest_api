49b463b8ffae8e4edd873885d99006f60e3dd278
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-03 02:01:12 +0600

    Require mongoose library instead of modified

--- models/todo.js
+++ models/todo.js
@@ -1,4 +1,4 @@
-const mongoose = require("../db/mongoose")
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const todoSchema = new Schema({
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require("../db/mongoose"),
-  Schema = mongoose.Schema
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   email: {
