c92a3eb94fa0c71de128cae4e6ca629a376c992a
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-02 19:31:07 +0600

    Add heroku config for mLab

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost:27017/todo-app-mongoose")
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/todo-app-mongoose"
+)
 
 module.exports = mongoose
