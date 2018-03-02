b37fa58cb7ef79251a9143c1fbedf755cbf09d6c
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-03 02:05:32 +0600

    Add controller utility for PATCH

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,4 +1,6 @@
 const { ObjectID } = require("mongodb")
+const pick = require("lodash/pick")
+const isBoolean = require("lodash/isBoolean")
 
 const ToDo = require("../models/todo")
 
@@ -39,13 +41,61 @@ exports.getToDoById = (req, res) => {
   const { id } = req.params
 
   if (!ObjectID.isValid(id))
-    return res.status(404).send({ error: "Invalid ID" })
+    return res.status(404).send({ message: "Invalid ID" })
 
   ToDo.findById(id)
     .then(todo => {
       return !todo
-        ? res.status(400).send({ message: "No todo found" })
-        : res.send(todo)
+        ? res
+            .status(400)
+            .send({ message: "Bad request! The todo cannot be found" })
+        : res.send({ message: "Todo found", todo })
     })
     .catch(err => res.status(404).send({ message: err.message }))
+}
+
+exports.deleteToDo = (req, res) => {
+  const { id } = req.params
+
+  if (!ObjectID.isValid(id))
+    return res.status(404).send({ message: "Invalid ID" })
+
+  ToDo.findByIdAndRemove(id)
+    .then(todo => {
+      return !todo
+        ? res
+            .status(400)
+            .send({ message: "Bad request! The todo cannot be found" })
+        : res.send({ message: "Successfully deleted the todo", todo })
+    })
+    .catch(err => {
+      res.status(404).send({ message: err.message })
+    })
+}
+
+exports.updateToDos = (req, res) => {
+  const { id } = req.params
+  const body = pick(req.body, ["todo", "completed"])
+
+  if (!ObjectID.isValid(id))
+    return res.status(404).send({ message: "Invalid ID" })
+
+  if (isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+
+  ToDo.findByIdAndUpdate(id, { $set: body }, { new: true, runValidators: true })
+    .then(todo => {
+      return !todo
+        ? res
+            .status(400)
+            .send({ message: "Bad request! The todo cannot be updated" })
+        : res.send({ message: "Successfully updated the todo", todo })
+    })
+    .catch(err => {
+      res.status(404).send({ message: err.message })
+    })
 }
