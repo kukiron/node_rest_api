57f591b324ac77123b1e289c77f5077fa3d032c7
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-04 01:15:41 +0600

    Add authenticate middleware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,17 @@
+const User = require("../models/user")
+
+module.exports = (req, res, next) => {
+  const token = req.header("x-auth")
+
+  User.findByToken(token)
+    .then(user => {
+      if (!user) return Promise.reject()
+
+      req.user = user
+      req.token = token
+      next()
+    })
+    .catch(err => {
+      res.status(401).send(err)
+    })
+}
