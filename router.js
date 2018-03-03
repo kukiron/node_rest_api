7f66ac0aee33b41be4d5ccd5e3da43501a9684dc
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-04 01:19:10 +0600

    Add logout route

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const todoController = require("./controllers/todo")
 const userController = require("./controllers/user")
+const authenticate = require("./middlewares/authenticate")
 
 module.exports = app => {
   app.get("/", (req, res) => {
@@ -10,7 +11,7 @@ module.exports = app => {
   app.get("/todos", todoController.getAllToDos)
   app.get("/users", userController.getAllUsers)
   app.get("/todos/:id", todoController.getToDoById)
-  app.get("/users/me", userController.getUserByToken)
+  app.get("/users/me", authenticate, userController.getUserByToken)
 
   // POST requests
   app.post("/todos/addtodo", todoController.addTodo)
@@ -20,6 +21,7 @@ module.exports = app => {
   // DELETE requests
   app.delete("/todos/:id", todoController.deleteToDo)
   app.delete("/users/:id", userController.deleteUser)
+  app.delete("/users/me/token", authenticate, userController.logoutUser)
 
   // PATCH requests
   app.patch("/todos/:id", todoController.updateToDos)
