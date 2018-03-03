095a77f1dcae0913505c540cf5ee97a7b1c07515
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-03 16:31:53 +0600

    Add methods for fetching & deleting user

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,38 @@
+const { ObjectID } = require("mongodb")
+const pick = require("lodash/pick")
+
 const User = require("../models/user")
 
 exports.signUpUser = (req, res, next) => {
-  const { email, password } = req.body
+  const { email } = req.body
 
   // See whether a user with a given email exists
   User.findOne({ email }, (err, existingUser) => {
     if (err) return next(err)
 
-    if (existingUser) return res.status(422).send({ error: "Email is in use" })
+    if (existingUser)
+      return res.status(422).send({ message: "Email is in use" })
 
     // If not, create a new user & save
-    const user = new User({
-      email,
-      password
-    })
+    const body = pick(req.body, ["email", "password"])
+    const user = new User(body)
 
-    user.save().then(
-      doc => {
-        res.send({
+    user
+      .save()
+      .then(() => user.generateToken())
+      .then(token => {
+        res.header("x-auth", token).send({
           message: "New user account is successfully creaed",
-          userObj: doc
+          userObj: user
         })
-      },
-      err => {
+      })
+      .catch(err => {
         res.status(400).send(err.message)
-      }
-    )
+      })
   })
 }
 
-exports.getUsers = (req, res) => {
+exports.getAllUsers = (req, res) => {
   User.find().then(
     users => {
       res.send({ users })
@@ -38,4 +41,38 @@ exports.getUsers = (req, res) => {
       res.status(400).send(err)
     }
   )
+}
+
+exports.getUserByToken = (req, res) => {
+  const token = req.header("x-auth")
+
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject()
+      }
+      res.send(user)
+    })
+    .catch(err => {
+      res.status(401).send(err)
+    })
+}
+
+exports.deleteUser = (req, res) => {
+  const { id } = req.params
+
+  if (!ObjectID.isValid(id))
+    return res.status(404).send({ message: "Invalid ID" })
+
+  User.findByIdAndRemove(id)
+    .then(user => {
+      return !user
+        ? res
+            .status(400)
+            .send({ message: "Bad request! The user cannot be found" })
+        : res.send({ message: "Successfully deleted the user", user })
+    })
+    .catch(err => {
+      res.status(404).send({ message: err.message })
+    })
 }
