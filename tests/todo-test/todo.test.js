0a2ae84f55789f967729a0b7597a58b63d8de492
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-03 23:41:56 +0600

    Refactor test files & add test for login

---
+++ tests/todo-test/todo.test.js
@@ -0,0 +1,171 @@
+const request = require("supertest")
+const { expect } = require("chai")
+const { ObjectID } = require("mongodb")
+
+const app = require("../../server")
+const ToDo = require("../../models/todo")
+const { sampleToDos, populateTodos } = require("./todo-data")
+
+describe("Requests --> Todo", () => {
+  beforeEach(populateTodos)
+
+  describe("POST '/todos/addtodo", () => {
+    it("should create a new todo", done => {
+      const todo = "New stuff to do"
+
+      request(app)
+        .post("/todos/addtodo")
+        .send({ todo })
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .expect(res => expect(res.body.todoItem.todo).to.equal(todo))
+        .end(err => {
+          err
+            ? done(err)
+            : ToDo.find({ todo })
+                .then(todos => {
+                  expect(todos.length).to.equal(1)
+                  expect(todos[0].todo).to.equal(todo)
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+
+    it("should not create a new todo with bad data", done => {
+      request(app)
+        .post("/todos/addtodo")
+        .send({ todo: "" })
+        .expect(400)
+        .expect("Content-Type", /html/)
+        .end(err => {
+          err
+            ? done(err)
+            : ToDo.find()
+                .then(todos => {
+                  expect(todos.length).to.equal(sampleToDos.length)
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+  })
+
+  describe("GET '/todos'", () => {
+    it("should get all the todos", done => {
+      request(app)
+        .get("/todos")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .expect(res =>
+          expect(res.body.todos.length).to.equal(sampleToDos.length)
+        )
+        .end(done)
+    })
+  })
+
+  describe("GET '/todos/:id'", () => {
+    it("should send a status of 404 for non-object ID", done => {
+      request(app)
+        .get("/todos/123")
+        .expect(res => expect(res.statusCode).to.equal(404))
+        .end(done)
+    })
+
+    it("should send a todo item", done => {
+      request(app)
+        .get(`/todos/${sampleToDos[0]._id}`)
+        .expect(200)
+        .expect("content-Type", /json/)
+        .expect(res => expect(res.body.todo.todo).to.equal(sampleToDos[0].todo))
+        .end(done)
+    })
+
+    it("should send 400 status for valid ID with no todo", done => {
+      request(app)
+        .get(`/todos/${new ObjectID().toHexString()}`)
+        .expect(res => expect(res.statusCode).to.equal(400))
+        .end(done)
+    })
+  })
+
+  describe("DELETE '/todos/:id'", () => {
+    it("should delete the todo with valid ID", done => {
+      const hexId = sampleToDos[1]._id.toHexString()
+
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect(res => expect(res.body.todo.todo).to.equal(sampleToDos[1].todo))
+        .end(err => {
+          err
+            ? done(err)
+            : ToDo.findById(hexId)
+                .then(todo => {
+                  expect(todo).to.be.a("null")
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+
+    it("should send a status of 404 for non-object ID", done => {
+      request(app)
+        .delete("/todos/123")
+        .expect(res => expect(res.statusCode).to.equal(404))
+        .end(done)
+    })
+
+    it("should send 400 status if todo not found", done => {
+      request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(res => expect(res.statusCode).to.equal(400))
+        .end(err => {
+          err
+            ? done(err)
+            : ToDo.find()
+                .then(todos => {
+                  expect(todos.length).to.equal(sampleToDos.length)
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+  })
+
+  describe("PATCH '/todos/:id'", () => {
+    it("should update the todo", done => {
+      const hexId = sampleToDos[0]._id.toHexString()
+      const body = { todo: "This todo is done", completed: true }
+
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .send(body)
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .expect(res => {
+          expect(res.body.todo.todo).to.equal(body.todo)
+          expect(res.body.todo.completed).to.equal(true)
+          expect(typeof res.body.todo.completedAt).to.equal("number")
+        })
+        .end(done)
+    })
+
+    it("should set completedAt to null when todo is not completed", done => {
+      const hexId = sampleToDos[1]._id.toHexString()
+      const body = { todo: "Finish this todo", completed: false }
+
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .send(body)
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .expect(res => {
+          expect(res.body.todo.todo).to.equal(body.todo)
+          expect(res.body.todo.completed).to.equal(false)
+          expect(res.body.todo.completedAt).to.be.a("null")
+        })
+        .end(done)
+    })
+  })
+})
---
+++ tests/user-test/user.test.js
@@ -0,0 +1,104 @@
+const request = require("supertest")
+const { expect } = require("chai")
+
+const app = require("../../server")
+const User = require("../../models/user")
+const { sampleUsers, populateUsers } = require("./user-data")
+
+describe("Requests --> User:", () => {
+  beforeEach(populateUsers)
+
+  describe("POST '/users/signup'", () => {
+    it("should create a new user account", done => {
+      const email = "example@yahoo.com"
+      const password = "password123"
+
+      request(app)
+        .post("/users/signup")
+        .send({ email, password })
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .expect(res => {
+          expect(res.header["x-auth"]).to.exist
+          expect(res.body.userObj._id).to.exist
+          expect(res.body.userObj.email).to.equal(email)
+        })
+        .end(err => {
+          err
+            ? done(err)
+            : User.find({ email })
+                .then(user => {
+                  expect(user).to.exist
+                  expect(user.password).to.not.equal(password)
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+
+    it("should return validation error if request is invalid", done => {
+      const email = "kafil.kiron"
+      const password = "0981"
+
+      request(app)
+        .post("/users/signup")
+        .send({ email, password })
+        .expect("Content-Type", /html/)
+        .expect(res => {
+          expect(res.statusCode).to.equal(400)
+          expect(res.body).to.be.an("object").that.is.empty
+        })
+        .end(done)
+    })
+
+    it("should not create a new user if email is in use", done => {
+      const email = sampleUsers[1].email
+      const password = "0000001"
+
+      request(app)
+        .post("/users/signup")
+        .send({ email, password })
+        .expect("Content-Type", /json/)
+        .expect(res => {
+          expect(res.statusCode).to.equal(422)
+          expect(res.body).to.eql({ message: "Email is in use" })
+        })
+        .end(err => {
+          err
+            ? done(err)
+            : User.find()
+                .then(users => {
+                  expect(users.length).to.equal(2)
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+  })
+
+  describe("GET '/users/me'", () => {
+    it("should return user if authenticated", done => {
+      request(app)
+        .get("/users/me")
+        .set("x-auth", sampleUsers[0].tokens[0].token)
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .expect(res => {
+          expect(res.body._id).to.equal(sampleUsers[0]._id.toHexString())
+          expect(res.body.email).to.equal(sampleUsers[0].email)
+        })
+        .end(done)
+    })
+
+    it("should return 401 if user is not authenticated", done => {
+      request(app)
+        .get("/users/me")
+        .expect("Content-Type", /html/)
+        .expect(res => {
+          expect(res.statusCode).to.equal(401)
+          expect(res.body).to.be.an("object").that.is.empty
+        })
+        .end(done)
+    })
+  })
+})
