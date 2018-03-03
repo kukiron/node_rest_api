9348fc87a69efa86a193c9b28b452c96858a5d34
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-03 16:34:27 +0600

    Add methods to User model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,11 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const pick = require("lodash/pick")
+
+const configSecret = require("../config/config-secret.js")
+
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -8,14 +15,83 @@ const userSchema = new Schema({
     minlength: 3,
     trim: true,
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
   },
   password: {
     type: String,
     required: true,
-    minlength: 4
-  }
+    minlength: [6, "The password should be atleast 6 characters long"]
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 })
+
+userSchema.pre("save", function(next) {
+  const user = this
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return next(err)
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err)
+
+        user.password = hash
+        next()
+      })
+    })
+  } else next()
+})
+
+userSchema.methods.toJSON = function() {
+  const user = this
+  const userObj = user.toObject()
+
+  return pick(userObj, ["_id", "email"])
+}
+
+userSchema.methods.generateToken = function() {
+  const user = this
+  const access = "auth"
+  const token = jwt
+    .sign({ _id: user._id.toHexString(), access }, configSecret.secret)
+    .toString()
+
+  user.tokens = user.tokens.concat([{ access, token }])
+
+  return user.save().then(() => token)
+}
+
+userSchema.statics.findByToken = function(token) {
+  const User = this
+  let decoded
+
+  try {
+    decoded = jwt.verify(token, configSecret.secret)
+  } catch (err) {
+    return Promise.reject(`User token error - ${err.message}`)
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
+  })
+}
 
 const User = mongoose.model("user", userSchema)
 
