9c49434f0d038e6ae522d08032971859279a34fd
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-04 19:18:36 +0600

    Change env variable for local test

--- config/config-env.js
+++ config/config-env.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV || "development"
 
-if (env === "development" || env === "test") {
+if (env === "development" || env === "test_local") {
   const config = require("./config.json")
   const envConfig = config[env]
 
