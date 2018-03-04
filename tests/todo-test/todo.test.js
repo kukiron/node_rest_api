84e9c1bf6b146bacfdafb5c1817364f0247a78e7
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-04 14:00:10 +0600

    Modify tests for authenticated user

--- tests/todo-test/todo.test.js
+++ tests/todo-test/todo.test.js
@@ -4,9 +4,15 @@ const { ObjectID } = require("mongodb")
 
 const app = require("../../server")
 const ToDo = require("../../models/todo")
-const { sampleToDos, populateTodos } = require("./todo-data")
+const {
+  sampleUsers,
+  populateUsers,
+  sampleToDos,
+  populateTodos
+} = require("../seed-data")
 
 describe("Requests --> Todo", () => {
+  beforeEach(populateUsers)
   beforeEach(populateTodos)
 
   describe("POST '/todos/addtodo", () => {
@@ -15,6 +21,7 @@ describe("Requests --> Todo", () => {
 
       request(app)
         .post("/todos/addtodo")
+        .set("x-auth", sampleUsers[0].tokens[0].token)
         .send({ todo })
         .expect(200)
         .expect("Content-Type", /json/)
@@ -35,6 +42,7 @@ describe("Requests --> Todo", () => {
     it("should not create a new todo with bad data", done => {
       request(app)
         .post("/todos/addtodo")
+        .set("x-auth", sampleUsers[0].tokens[0].token)
         .send({ todo: "" })
         .expect(400)
         .expect("Content-Type", /html/)
@@ -55,11 +63,10 @@ describe("Requests --> Todo", () => {
     it("should get all the todos", done => {
       request(app)
         .get("/todos")
+        .set("x-auth", sampleUsers[0].tokens[0].token)
         .expect(200)
         .expect("Content-Type", /json/)
-        .expect(res =>
-          expect(res.body.todos.length).to.equal(sampleToDos.length)
-        )
+        .expect(res => expect(res.body.todos.length).to.equal(1))
         .end(done)
     })
   })
@@ -68,6 +75,7 @@ describe("Requests --> Todo", () => {
     it("should send a status of 404 for non-object ID", done => {
       request(app)
         .get("/todos/123")
+        .set("x-auth", sampleUsers[1].tokens[0].token)
         .expect(res => expect(res.statusCode).to.equal(404))
         .end(done)
     })
@@ -75,15 +83,26 @@ describe("Requests --> Todo", () => {
     it("should send a todo item", done => {
       request(app)
         .get(`/todos/${sampleToDos[0]._id}`)
+        .set("x-auth", sampleUsers[0].tokens[0].token)
         .expect(200)
         .expect("content-Type", /json/)
         .expect(res => expect(res.body.todo.todo).to.equal(sampleToDos[0].todo))
         .end(done)
     })
 
+    it("should NOT return a todo item created by other user", done => {
+      request(app)
+        .get(`/todos/${sampleToDos[0]._id}`)
+        .set("x-auth", sampleUsers[1].tokens[0].token)
+        .expect("content-Type", /json/)
+        .expect(res => expect(res.statusCode).to.equal(400))
+        .end(done)
+    })
+
     it("should send 400 status for valid ID with no todo", done => {
       request(app)
         .get(`/todos/${new ObjectID().toHexString()}`)
+        .set("x-auth", sampleUsers[0].tokens[0].token)
         .expect(res => expect(res.statusCode).to.equal(400))
         .end(done)
     })
@@ -95,6 +114,7 @@ describe("Requests --> Todo", () => {
 
       request(app)
         .delete(`/todos/${hexId}`)
+        .set("x-auth", sampleUsers[1].tokens[0].token)
         .expect(200)
         .expect(res => expect(res.body.todo.todo).to.equal(sampleToDos[1].todo))
         .end(err => {
@@ -109,9 +129,30 @@ describe("Requests --> Todo", () => {
         })
     })
 
+    it("should NOT delete todo of other user", done => {
+      const hexId = sampleToDos[0]._id.toHexString()
+
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .set("x-auth", sampleUsers[1].tokens[0].token)
+        .expect("Content-Type", /json/)
+        .expect(res => expect(res.statusCode).to.equal(400))
+        .end(err => {
+          err
+            ? done(err)
+            : ToDo.findById(hexId)
+                .then(todo => {
+                  expect(todo.todo).to.equal(sampleToDos[0].todo)
+                  done()
+                })
+                .catch(err => done(err))
+        })
+    })
+
     it("should send a status of 404 for non-object ID", done => {
       request(app)
         .delete("/todos/123")
+        .set("x-auth", sampleUsers[1].tokens[0].token)
         .expect(res => expect(res.statusCode).to.equal(404))
         .end(done)
     })
@@ -119,6 +160,7 @@ describe("Requests --> Todo", () => {
     it("should send 400 status if todo not found", done => {
       request(app)
         .delete(`/todos/${new ObjectID().toHexString()}`)
+        .set("x-auth", sampleUsers[1].tokens[0].token)
         .expect(res => expect(res.statusCode).to.equal(400))
         .end(err => {
           err
@@ -140,6 +182,7 @@ describe("Requests --> Todo", () => {
 
       request(app)
         .patch(`/todos/${hexId}`)
+        .set("x-auth", sampleUsers[0].tokens[0].token)
         .send(body)
         .expect(200)
         .expect("Content-Type", /json/)
@@ -151,12 +194,26 @@ describe("Requests --> Todo", () => {
         .end(done)
     })
 
+    it("should NOT update the todo", done => {
+      const hexId = sampleToDos[0]._id.toHexString()
+      const body = { todo: "This todo is done", completed: true }
+
+      request(app)
+        .patch(`/todos/${hexId}`)
+        .set("x-auth", sampleUsers[1].tokens[0].token)
+        .send(body)
+        .expect("Content-Type", /json/)
+        .expect(res => expect(res.statusCode).to.equal(400))
+        .end(done)
+    })
+
     it("should set completedAt to null when todo is not completed", done => {
       const hexId = sampleToDos[1]._id.toHexString()
       const body = { todo: "Finish this todo", completed: false }
 
       request(app)
         .patch(`/todos/${hexId}`)
+        .set("x-auth", sampleUsers[1].tokens[0].token)
         .send(body)
         .expect(200)
         .expect("Content-Type", /json/)
