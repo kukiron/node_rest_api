c041f99910cac616e09a18f87d41145a29bfefc1
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-03-06 10:31:17 +0600

    Restructure routes based on authentication

--- router.js
+++ router.js
@@ -4,25 +4,23 @@ const authenticate = require("./middlewares/authenticate")
 
 module.exports = app => {
   app.get("/", (req, res) => {
-    res.send("Hi there!")
+    res.send("Welcome to the REST API server!")
   })
 
-  // GET requests
+  /** Requests with authentication **/
   app.get("/todos", authenticate, todoController.getAllToDos)
-  app.get("/users", userController.getAllUsers)
   app.get("/todos/:id", authenticate, todoController.getToDoById)
-  app.get("/users/me", authenticate, userController.getUserByToken)
-
-  // POST requests
+  app.get("/users/me", authenticate, userController.getUserByToken) // User's account/dashboard route
   app.post("/todos/addtodo", authenticate, todoController.addTodo)
+  app.delete("/todos/:id", authenticate, todoController.deleteToDo)
+  app.delete("/users/me/token", authenticate, userController.logoutUser)
+  app.patch("/todos/:id", authenticate, todoController.updateToDos)
+
+  /** Routes generating auth token **/
   app.post("/users/signup", userController.signUpUser)
   app.post("/users/login", userController.loginUser)
 
-  // DELETE requests
-  app.delete("/todos/:id", authenticate, todoController.deleteToDo)
+  /** Routes for test purposes **/
+  app.get("/users", userController.getAllUsers)
   app.delete("/users/:id", userController.deleteUser)
-  app.delete("/users/me/token", authenticate, userController.logoutUser)
-
-  // PATCH requests
-  app.patch("/todos/:id", authenticate, todoController.updateToDos)
 }
